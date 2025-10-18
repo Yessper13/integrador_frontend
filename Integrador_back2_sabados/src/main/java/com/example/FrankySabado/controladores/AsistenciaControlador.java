e7ecbd1d919041ad531682a190f64645dc223e6a
JuanEstebanHoy0s <jehz.14@hotmail.com>
2025-10-18 09:54:55 -0500

    version de prueba 0

--- Integrador_back2_sabados/src/main/java/com/example/FrankySabado/controladores/AsistenciaControlador.java
+++ Integrador_back2_sabados/src/main/java/com/example/FrankySabado/controladores/AsistenciaControlador.java
@@ -1,4 +1,60 @@
 package com.example.FrankySabado.controladores;
 
+
+import com.example.FrankySabado.modelos.Asistencia;
+import com.example.FrankySabado.servicios.AsistenciaServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/asistencias")
 public class AsistenciaControlador {
+    @Autowired
+    AsistenciaServicio servicio;
+
+    //1. Activar API para buscar asistencia por estado
+    @GetMapping
+    public ResponseEntity<?> activarBuscarEstado(@PathVariable String estado) {
+        try {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(this.servicio.buscarEstado(estado));
+        } catch (Exception error) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(error.getMessage());
+        }
+    }
+
+    //2. Activar API para buscar buscar por fecha
+    @GetMapping
+    public ResponseEntity<?> activarBuscarPorFecha(@RequestBody LocalDate fecha) {
+        try {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(this.servicio.buscarPorFecha(fecha));
+        } catch (Exception error) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(error.getMessage());
+        }
+    }
+
+    //3. Activar API buscar por grupo
+    @GetMapping
+    public ResponseEntity<?> activarBuscarPorGrupo(@PathVariable Integer idGrupo) {
+        try {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(this.servicio.buscarPorGrupo(idGrupo));
+        } catch (Exception error) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(error.getMessage());
+        }
+    }
 }
--- Integrador_back2_sabados/src/main/java/com/example/FrankySabado/modelos/mapas/IMapaAsistencia.java
+++ Integrador_back2_sabados/src/main/java/com/example/FrankySabado/modelos/mapas/IMapaAsistencia.java
@@ -3,12 +3,12 @@ package com.example.FrankySabado.modelos.mapas;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import com.example.FrankySabado.modelos.Asistencia;
-import com.example.FrankySabado.modelos.dtos.AsisteciaDTO;
+import com.example.FrankySabado.modelos.dtos.AsistenciaDTO;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface ImapaAsistencia {
+public interface IMapaAsistencia {
 
 
 
@@ -17,9 +17,9 @@ public interface ImapaAsistencia {
     @Mapping(source = "observacion", target = "observacion")
     @Mapping(source = "fecha", target = "fecha")
     @Mapping(source = "idGrupo", target = "idGrupo")
-    AsisteciaDTO convertirModeloADTO(Asistencia asistencia);
+    AsistenciaDTO convertirModeloADTO(Asistencia asistencia);
 
-    List<AsisteciaDTO> convertir_lista_a_dto(List<Asistencia> lista);
+    List<AsistenciaDTO> convertir_lista_a_dto(List<Asistencia> lista);
 
 
 }
