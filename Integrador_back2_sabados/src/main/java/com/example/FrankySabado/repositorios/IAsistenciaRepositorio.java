8d7d4ab5e9c02ba77d8f224a8b0cb4b843c1fd74
JUJOSANE <jujosane888@gmail.com>
2025-10-18 09:18:31 -0500

    Feat: creacion del repositorio de asistencia

---
+++ Integrador_back2_sabados/src/main/java/com/example/FrankySabado/repositorios/IAsistenciaRepositorio.java
@@ -0,0 +1,21 @@
+package com.example.FrankySabado.repositorios;
+
+import com.example.FrankySabado.modelos.Asistencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface IAsistenciaRepositorio extends JpaRepository<Asistencia, Integer> {
+
+    List<Asistencia> findByEstado(String estado);
+    List<Asistencia> findByFecha(LocalDate fecha);
+
+
+    //Consulta para IdAsistencia
+    List<Asistencia>findByGrupo(Integer idGrupo);
+
+
+}
